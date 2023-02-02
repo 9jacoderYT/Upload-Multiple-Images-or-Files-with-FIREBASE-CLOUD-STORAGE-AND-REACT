757361abe6c0dcb828c59ddc71dfbade4e33cef9
Smart Oghenemine <35765212+VinciDemons@users.noreply.github.com>
2023-02-02 15:00:19 +0100

    First commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,88 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { useState } from "react";
+
+// Email Validation
+const isEmail = (email) =>
+  /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(email);
+
+const isPassword = (password) => {
+  if (!password || password.length < 7 || password.length > 15) {
+    return false;
+  }
+
+  return true;
+};
+
+const isEmpty = (value) => {
+  if (!value || value.length < 5 || value.length > 30) {
+    return false;
+  } else {
+    return true;
+  }
+};
 
 function App() {
+  const [nameInput, setNameInput] = useState();
+  const [emailInput, setEmailInput] = useState();
+  const [passwordInput, setPasswordInput] = useState();
+  const [error, setError] = useState();
+  const [success, setSuccess] = useState();
+
+  const handleSubmit = () => {
+    // validation of inputs
+
+    // Email Validation
+    if (!isEmail(emailInput)) {
+      setError("Your Email is Invalid");
+      return;
+    }
+
+    // Password validation
+    if(!isPassword(passwordInput)){
+      setError("Your Password is not valid");
+      return;
+    }
+
+    setError(null);
+    setSuccess("Your fields are valid, and have been submitted")
+    return;
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <p>
+        <label> Name : </label>
+        <input
+          type="text"
+          value={nameInput}
+          onChange={(event) => setNameInput(event.target.value)}
+        />
+        <br />
+        <label> Email : </label>
+        <input
+          type="text"
+          value={emailInput}
+          onChange={(event) => setEmailInput(event.target.value)}
+        />
+        <br />
+        <label> Password : </label>
+        <input
+          type="text"
+          value={passwordInput}
+          onChange={(event) => setPasswordInput(event.target.value)}
+        />
+
+        <p>{error && <p>{error}</p>}</p>
+        <p>{success && <p>{success}</p>}</p>
+        
+
+        <br />
+        <button onClick={handleSubmit}>SUBMIT</button>
+      </p>
     </div>
   );
 }
 
 export default App;
+
+// Thank you for watching, pls subscribe
---
+++ src/data.js
@@ -0,0 +1,70 @@
+const data = [
+  {
+    name: "Norris",
+    age: "27",
+    gender: "male",
+    verified: true,
+  },
+  {
+    name: "Rachel",
+    age: "21",
+    gender: "female",
+    verified: true,
+  },
+  {
+    name: "Sainz",
+    age: "26",
+    gender: "male",
+    verified: false,
+  },
+  {
+    name: "Augusta",
+    age: "40",
+    gender: "female",
+    verified: true,
+  },
+  {
+    name: "Hamilton",
+    age: "35",
+    gender: "male",
+    verified: true,
+  },
+  {
+    name: "Max",
+    age: "24",
+    gender: "male",
+    verified: false,
+  },
+  {
+    name: "Christina",
+    age: "22",
+    gender: "female",
+    verified: true,
+  },
+  {
+    name: "Kayode",
+    age: "33",
+    gender: "male",
+    verified: true,
+  },
+  {
+    name: "Frank",
+    age: "13",
+    gender: "male",
+    verified: false,
+  },
+  {
+    name: "Anita",
+    age: "25",
+    gender: "female",
+    verified: true,
+  },
+  {
+    name: "James",
+    age: "21",
+    gender: "male",
+    verified: false,
+  },
+];
+
+export default data;
